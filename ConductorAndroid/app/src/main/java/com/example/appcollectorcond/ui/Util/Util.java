78bfb3a7c59a3afdb1ea202f043726945749bafa
Diana3467 <72553467@continental.edu.pe>
2022-06-09 21:54:27 -0500

    update ip

--- ConductorAndroid/app/src/main/java/com/example/appcollectorcond/ui/Util/Util.java
+++ ConductorAndroid/app/src/main/java/com/example/appcollectorcond/ui/Util/Util.java
@@ -2,7 +2,8 @@ package com.example.appcollectorcond.ui.Util;
 
 public class Util {
 
-    public static String url = "http://192.168.1.60:8083/api/";
+    //public static String url = "http://192.168.1.60:8083/api/";
+    public static String url = "http://192.168.18.12:8083/api/";
 
     public static final String ARCHIVO_PREFRENCIAS = "Preferencias_UserConductor";
 }
