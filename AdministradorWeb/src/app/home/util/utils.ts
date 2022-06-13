c2c8ba2e5bac7caec7b006246b1f1a700503a970
Diana3467 <72553467@continental.edu.pe>
2022-06-13 01:22:25 -0500

    Enrutando servidor remoto

--- AdministradorWeb/src/app/home/util/utils.ts
+++ AdministradorWeb/src/app/home/util/utils.ts
@@ -1,4 +1,5 @@
 export class Utils {
     //public static url:string = 'http://localhost:7570/';
-    public static url:string = 'http://192.168.1.60:8083/';
+    //public static url:string = 'http://192.168.1.60:8083/';
+    public static url:string = 'http://144.22.57.33/';
 }
